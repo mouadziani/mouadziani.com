c7568ec3074effebf0bcb4c031c1e29b1e3b36ba
mouadziani <mouad.ziani1997@gmail.com>
2021-03-05 01:21:23 +0100

    feat(ui): added hero section & upgrade tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400&display=swap' },
     ]
   },
 
