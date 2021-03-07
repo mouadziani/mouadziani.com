1496364e31dfadd8ca68b662f9752a28a8649d46
mouadziani <mouad.ziani1997@gmail.com>
2021-03-07 19:53:13 +0100

    feat(ui): reafctring skills section

--- components/home/Skills.vue
+++ components/home/Skills.vue
@@ -11,18 +11,13 @@
         </div>
         <div class="grid grid-cols-3 gap-6">
             <div 
-              class="block bg-white px-6 py-4 shadow rounded-lg mt-10 lg:mt-0 w-full"
+              class="block bg-white p-6 shadow rounded-lg mt-10 lg:mt-0 w-full"
               v-for="(skill, index) in skills"
               :key="index"
             >
-              <!-- <div class="flex items-center justify-center h-10 w-10 rounded-md bg-green-700 text-white">
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"></path>
-                </svg>
-              </div> -->
-              <div class="mt-3">
+              <div>
                 <h5 class="text-lg leading-6 font-medium text-gray-900">{{ skill.title }}</h5>
-                <p class="mt-2 mb-4 text-base leading-6 text-gray-500">Rendez-vous sur le forum pour discuter de tout ce qui concerne le code/design.</p>
+                <p class="mt-2 mb-4 text-base leading-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 <nav
                   class="flex flex-col sm:text-left space-y-2"
                 >
