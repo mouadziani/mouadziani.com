2fecab9da2c0794bb9a5ae5e5ad9e2f559610bc1
MouadZIANI <mouad.ziani1997@gmail.com>
2021-03-16 19:19:44 +0100

    feat: change meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Mouad ZIANI - Fullstack Software Enginner',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Personal website of mouad ziani',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
